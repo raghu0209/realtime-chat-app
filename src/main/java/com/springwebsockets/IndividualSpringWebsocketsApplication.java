a9eefc5579ab27feb68010ba26b0e722736fa13d
Raghavendra Dasari <162073459+raghu0209@users.noreply.github.com>
2026-04-07 22:25:21 +0530

    Add files via upload

---
+++ src/main/java/com/springwebsockets/IndividualSpringWebsocketsApplication.java
@@ -0,0 +1,13 @@
+package com.springwebsockets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndividualSpringWebsocketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndividualSpringWebsocketsApplication.class, args);
+	}
+
+}
